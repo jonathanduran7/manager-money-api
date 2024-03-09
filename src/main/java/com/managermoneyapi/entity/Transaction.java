ecebbca6acb2e96a9c673028b9dd8dec46c6f140
jonathanduran7 <joniduran624@gmail.com>
2024-03-09 14:50:58 -0300

    create transaction entity

---
+++ src/main/java/com/managermoneyapi/entity/Transaction.java
@@ -0,0 +1,36 @@
+package com.managermoneyapi.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+
+@Entity
+@Table(name = "transaction")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class Transaction {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "title", nullable = false)
+    private String title;
+
+    @Column(name = "description", nullable = false)
+    private String description;
+
+    @Column(name = "type_transaction", nullable = false)
+    private String type_transaction;
+
+    @Column(name = "created_at", nullable = false)
+    private String created_at;
+
+    @Column(name = "amount", nullable = false)
+    private BigDecimal amount;
+}
