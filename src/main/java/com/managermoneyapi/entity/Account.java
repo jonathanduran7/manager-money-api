568b9f09f0852114cbf3b8941b07178f92691a31
jonathanduran7 <joniduran624@gmail.com>
2024-03-09 14:36:47 -0300

    create account entity

---
+++ src/main/java/com/managermoneyapi/entity/Account.java
@@ -0,0 +1,24 @@
+package com.managermoneyapi.entity;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "account")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class Account {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    @Column(name = "created_at", nullable = false, columnDefinition = "TIMESTAMP DEFAULT CURRENT_TIMESTAMP")
+    private String created_at;
+}
