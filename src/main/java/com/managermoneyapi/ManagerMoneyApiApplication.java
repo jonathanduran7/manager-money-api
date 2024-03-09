9b170d356767ca4e49265f274651aa475fce7de8
jonathanduran7 <joniduran624@gmail.com>
2024-03-09 14:36:14 -0300

    first commit

---
+++ src/main/java/com/managermoneyapi/ManagerMoneyApiApplication.java
@@ -0,0 +1,11 @@
+package com.managermoneyapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManagerMoneyApiApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(ManagerMoneyApiApplication.class, args);
+	}
+}
---
+++ src/test/java/com/managermoneyapi/ManagerMoneyApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.managermoneyapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ManagerMoneyApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
