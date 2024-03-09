c04dbc9e474ac1a1daf0ef602dd85b6b85f7225d
jonathanduran7 <joniduran624@gmail.com>
2024-03-09 15:00:55 -0300

    create account balance entity

---
+++ src/main/java/com/managermoneyapi/entity/AccountBalance.java
@@ -0,0 +1,28 @@
+package com.managermoneyapi.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+
+@Entity
+@Table(name = "account_balance")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class AccountBalance {
+    @Id
+    @GeneratedValue(strategy = jakarta.persistence.GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "balance", nullable = false)
+    private BigDecimal balance;
+
+    @OneToOne
+    @JoinColumn(name = "account_id", referencedColumnName = "id")
+    private Account account;
+}
