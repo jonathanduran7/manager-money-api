c0cbcd947d1e53a63614dd167179856e467cb279
jonathanduran7 <joniduran624@gmail.com>
2024-03-11 11:31:16 -0300

    add(account): repository interface

---
+++ src/main/java/com/managermoneyapi/repositories/AccountRepository.java
@@ -0,0 +1,9 @@
+package com.managermoneyapi.repositories;
+
+import com.managermoneyapi.entity.Account;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AccountRepository extends JpaRepository<Account, Long> {
+}
