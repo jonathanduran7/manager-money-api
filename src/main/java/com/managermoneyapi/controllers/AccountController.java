eec1cb132629e25654276f66aa323151e3aed215
jonathanduran7 <joniduran624@gmail.com>
2024-03-12 12:48:30 -0300

    add(account): modify methods in controllers

--- src/main/java/com/managermoneyapi/controllers/AccountController.java
+++ src/main/java/com/managermoneyapi/controllers/AccountController.java
@@ -1,15 +1,21 @@
 package com.managermoneyapi.controllers;
 
+import com.managermoneyapi.dto.CreateAccountDto;
 import com.managermoneyapi.entity.Account;
 import com.managermoneyapi.services.AccountService;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @Controller
 @RequestMapping("/accounts")
+@Validated
 public class AccountController {
 
     @Autowired
@@ -23,19 +29,28 @@ public class AccountController {
 
     @GetMapping("/{id}")
     @ResponseBody
-    public String show() {
-        return "account show";
+    public ResponseEntity<?> show(@PathVariable Long id) {
+        Account account = accountService.findById(id);
+        if (account != null) {
+            return ResponseEntity.ok(account);
+        } else {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Account not found");
+        }
     }
 
     @PostMapping
     @ResponseBody
-    public String create() {
-        return "account create";
+    public ResponseEntity<Account> create(@RequestBody @Valid CreateAccountDto accountDto) {
+        return ResponseEntity.status(HttpStatus.CREATED).body(accountService.save(accountDto));
     }
 
     @PutMapping("/{id}")
     @ResponseBody
-    public String update(@PathVariable Long id) {
-        return "account update" + id;
+    public ResponseEntity<?> update(@PathVariable Long id, @RequestBody @Valid CreateAccountDto accountDto) {
+        Account account = accountService.update(id, accountDto);
+        if(account == null) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Account not found");
+        }
+        return ResponseEntity.ok(account);
     }
 }
