acf4f59a2eb77f338bcf93d7aa57d76a9ced156a
jonathanduran7 <joniduran624@gmail.com>
2024-03-13 11:45:26 -0300

    feat(account): builder anotations dto

--- src/main/java/com/managermoneyapi/dto/CreateAccountDto.java
+++ src/main/java/com/managermoneyapi/dto/CreateAccountDto.java
@@ -2,9 +2,13 @@ package com.managermoneyapi.dto;
 
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 
 @Data
+@Builder
+@AllArgsConstructor
 public class CreateAccountDto {
     @Size(min = 3, message = "Name must have at least 3 characters")
     @NotBlank(message = "Name is required")
