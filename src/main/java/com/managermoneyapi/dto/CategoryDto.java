c14200f9432b86d71f8be3b8ef00d89074c1b299
jonathanduran7 <joniduran624@gmail.com>
2024-03-14 12:33:17 -0300

    add(category): create dto

---
+++ src/main/java/com/managermoneyapi/dto/CategoryDto.java
@@ -0,0 +1,18 @@
+package com.managermoneyapi.dto;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@Builder
+@NoArgsConstructor
+public class CategoryDto {
+    @Size(min = 3, message = "Name must have at least 3 characters")
+    @NotBlank(message = "Name is required")
+    private String name;
+}
