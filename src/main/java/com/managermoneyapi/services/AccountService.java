9847a522caf9ca81305d4b77bc6ac268ef2db5c4
jonathanduran7 <joniduran624@gmail.com>
2024-03-15 09:12:38 -0300

    add(account): implement base service for account service

--- src/main/java/com/managermoneyapi/services/AccountService.java
+++ src/main/java/com/managermoneyapi/services/AccountService.java
@@ -5,12 +5,4 @@ import com.managermoneyapi.entity.Account;
 
 import java.util.List;
 
-public interface AccountService {
-    List<Account> findAll();
-
-    Account findById(Long id);
-
-    Account save(CreateAccountDto accountDto);
-
-    Account update(Long id, CreateAccountDto accountDto);
-}
+public interface AccountService extends BaseService<Account, CreateAccountDto>{ }
