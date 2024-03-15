68f3338bd26c5d3db616f55e9f58a252b94f7ba1
jonathanduran7 <joniduran624@gmail.com>
2024-03-15 11:09:52 -0300

    refactor(category): implement base service

--- src/main/java/com/managermoneyapi/services/CategoryService.java
+++ src/main/java/com/managermoneyapi/services/CategoryService.java
@@ -6,10 +6,5 @@ import com.managermoneyapi.entity.Category;
 import java.util.List;
 import java.util.Optional;
 
-public interface CategoryService {
-    List<Category> findAll();
-    Optional<Category> findById(Long id);
-    Category save(CategoryDto categoryDto);
-    Optional<Category> update(Long id, CategoryDto categoryDto);
-    void delete(Long id);
+public interface CategoryService extends BaseService<Category, CategoryDto>{
 }
