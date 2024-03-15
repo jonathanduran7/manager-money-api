2bc4b903a1f4baa68839af94ac218ac5b08e138a
jonathanduran7 <joniduran624@gmail.com>
2024-03-15 09:09:45 -0300

    fix(account): add NoArgsConstructor decoration in dto

--- src/main/java/com/managermoneyapi/dto/CreateAccountDto.java
+++ src/main/java/com/managermoneyapi/dto/CreateAccountDto.java
@@ -5,10 +5,12 @@ import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @Builder
 @AllArgsConstructor
+@NoArgsConstructor
 public class CreateAccountDto {
     @Size(min = 3, message = "Name must have at least 3 characters")
     @NotBlank(message = "Name is required")
