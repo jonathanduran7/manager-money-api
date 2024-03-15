a71bfee61d03cc00d47a543f180679f6817fea7a
jonathanduran7 <joniduran624@gmail.com>
2024-03-15 11:11:34 -0300

    refactor(category): remove unnecessary imports

--- src/main/java/com/managermoneyapi/controllers/CategoryController.java
+++ src/main/java/com/managermoneyapi/controllers/CategoryController.java
@@ -3,14 +3,10 @@ package com.managermoneyapi.controllers;
 import com.managermoneyapi.dto.CategoryDto;
 import com.managermoneyapi.entity.Category;
 import com.managermoneyapi.services.CategoryService;
-import jakarta.validation.Valid;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
-import java.util.Optional;
 
 @Controller
 @RequestMapping("/categories")
