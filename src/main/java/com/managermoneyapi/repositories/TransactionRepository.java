1945be010faccfba612dfdd6188b135b3063fe77
jonathanduran7 <joniduran624@gmail.com>
2024-03-19 11:40:57 -0300

    feat(transaction): add repository

---
+++ src/main/java/com/managermoneyapi/repositories/TransactionRepository.java
@@ -0,0 +1,9 @@
+package com.managermoneyapi.repositories;
+
+import com.managermoneyapi.entity.Transaction;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TransactionRepository extends JpaRepository<Transaction, Long> {
+}
