340adbcc7a678ff21017bdb9e56e6f059fcf7d79
jonathanduran7 <joniduran624@gmail.com>
2024-03-19 21:31:18 -0300

    feat(transfer): add repository

---
+++ src/main/java/com/managermoneyapi/repositories/TransferRepository.java
@@ -0,0 +1,9 @@
+package com.managermoneyapi.repositories;
+
+import com.managermoneyapi.entity.Transfer;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TransferRepository extends JpaRepository<Transfer, Long> {
+}
