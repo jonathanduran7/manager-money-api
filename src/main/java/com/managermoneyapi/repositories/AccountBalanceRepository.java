aeb301ab4f563cfaee12b3f37bc038bb06ed8e2b
jonathanduran7 <joniduran624@gmail.com>
2024-04-02 16:11:58 -0300

    feat(accountBalance): add function for get account by id

--- src/main/java/com/managermoneyapi/repositories/AccountBalanceRepository.java
+++ src/main/java/com/managermoneyapi/repositories/AccountBalanceRepository.java
@@ -2,8 +2,14 @@ package com.managermoneyapi.repositories;
 
 import com.managermoneyapi.entity.AccountBalance;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
 
 @Repository
 public interface AccountBalanceRepository extends JpaRepository<AccountBalance, Long> {
+    @Query("SELECT a FROM AccountBalance a WHERE a.account.id = ?1")
+    public Optional<AccountBalance> findByAccountId(Long accountId);
+
 }
